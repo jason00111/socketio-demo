925ccc17fc9ed169248967aa97c097f3b4fbb671
jason00111 <emberleyjason@gmail.com>
2017-02-25 16:28:36 -0800

    Set up socket.io demo

---
+++ Readme.md
@@ -0,0 +1,19 @@
+>Socket.io is a powerful JavaScript library for realtime web applications. It enables realtime, bi-directional communication between web clients and servers. It has two parts: a client-side library that runs in the browser, and a server-side library for Node.js. Both components have a nearly identical API.
+-wikipedia
+
+Socket.io uses the WebSocket protocol which is built on the TCP protocol (HTTP is also usually built on the TCP protocol)
+
+This demo uses express but express is not required to use socket.io.  Check out the following links for more information and other ways to use socket.io.
+
+https://www.npmjs.com/package/socket.io
+https://github.com/socketio/socket.io
+
+
+There are comments in the files `server.js` and `index.html` which explain what is happening.
+
+To try it yourself, clone this repo and type:
+
+`npm i`
+`npm start`
+
+to start the server and then open `localhost:3000` in multiple browser windows at the same time.
---
+++ server.js
@@ -0,0 +1,47 @@
+var app = require('express')()
+var server = require('http').createServer(app);
+var io = require('socket.io')(server)
+
+app.get('/', function (request, response) {
+  response.sendFile(__dirname + '/index.html')
+})
+
+// This sets up a listener for a 'connection' message
+// which is automatically sent from the client when a new client connects
+io.on('connection', function (client) {
+  // This function gets run whenever a 'connection' message is recieved
+  // 'client' is an object representing a particular client
+  console.log('A client connected')
+
+  // Send a 'welcome' message to that particular client
+  client.emit('welcome')
+
+  // Send a 'newClientConnected' message to all clients
+  // The client id is sent along with the message as data
+  io.emit('newClientConnected', client.id)
+
+  // This sets up a listener for the 'newClientConnected' message
+  client.on('newClientConnected', function (data) {
+    // This function gets run whenever a 'newClientConnected' message is recieved
+    console.log('A new client with id', data, 'connected to the server')
+  })
+
+  // This sets up a listener for a 'makeAnnoucement' message
+  client.on('makeAnnouncement', function (data) {
+    // This function gets run whenever a 'makeAnnoucement' message is recieved
+    // 'data' is the data that was sent along with the message
+
+    // Send an 'announcement' message to all clients with data 'data'
+    io.emit('announcement', data)
+  })
+
+  // This sets up a listener for a 'disconnect' message
+  // which is automatically sent when a client disconnects
+  client.on('disconnect', function () {
+    // Send a 'clientDisconnected' message to all clients
+    // The id of the client who disconnected is sent as data with the message
+    io.emit('clientDisconnected', client.id)
+  })
+})
+
+server.listen(3000)
